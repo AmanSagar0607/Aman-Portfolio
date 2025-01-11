e0b6a0f79531a100764bd71e5430011fb0530297
Aman Sagar <amansagar0307@gmail.com>
2025-01-11 18:27:56 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,343 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  // { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+  { name: "Experience", link: "#experience" }, // Added Experience section
+  { name: "Resume", link: "https://drive.google.com/file/d/1XMqUP0G0J438yvha2rIYuwqt7vWvfjFP/view?usp=sharing" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a Digital Product Selling MarketPlace",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "SellBetter",
+    description: "Developed a scalable marketplace using Axios APIs, Cloudinary, and PayPal integration. Boosted performance by 30% through real-time analytics and optimized SDLC processes. Implemented advanced search filtering with responsive design for seamless user experience.",
+    img: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736592347/Portfolio-AmanSagar.in/Sellbetter_ghvsxm.jpg?_s=public-apps",
+    liveLink: "https://live-link.com",
+    sourceCodeLink: "https://github.com/yourusername/sellbetter",
+    // tools: ["Next.js", "PostgreSQL", "TailwindCSS"],
+    iconLists: ["https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593215/Portfolio-AmanSagar.in/nextjs-icon-dark-background_n4mrae.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593615/Portfolio-AmanSagar.in/669fbc4778158e8606eb3e83_favicon_5B1_5D_qxkli0.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593642/Portfolio-AmanSagar.in/Tailwind_CSS_Logo_urf3an.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593954/Portfolio-AmanSagar.in/typescript_original_logo_icon_146317_zekvqp.jpg?_s=public-apps"], // Example icon list
+  },
+  {
+    id: 2,
+    title: "ToolHunt",
+    description: "Elevate Your Projects with the Best Tools for Designers & Developers.",
+    img: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736592481/Portfolio-AmanSagar.in/ToolHunt.jpg?_s=public-apps",
+    liveLink: "https://live-link.com",
+    sourceCodeLink: "https://github.com/yourusername/toolhunt",
+    // tools: ["ReactJS", "Vercel", "NodeJS", "TailwindCSS", "JavaScript", "HTML", "CSS", "Git/Github"],
+    iconLists: ["https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593563/Portfolio-AmanSagar.in/React.js_logo-512_k7bq3r.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593885/Portfolio-AmanSagar.in/node-js-icon-454x512-nztofx17_zszuts.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593942/Portfolio-AmanSagar.in/1024px-Unofficial_JavaScript_logo_2.svg_tjajtk.jpg?_s=public-apps"], // Example icon list
+  },
+  {
+    id: 3,
+    title: "FigPro",
+    description: "Real-time Figma Clone: A collaborative design tool with real-time design features to deliver highly dynamic user experiences.",
+    img: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736592643/Portfolio-AmanSagar.in/FigPro.jpg?_s=public-apps",
+    liveLink: "https://live-link.com",
+    sourceCodeLink: "https://github.com/yourusername/figpro",
+    // tools: ["NextJS", "ReactJS", "Liveblocks", "FabricJS", "NodeJS", "TailwindCSS", "JavaScript", "TypeScript", "HTML", "CSS", "Git/Github"],
+    iconLists: ["https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736594202/Portfolio-AmanSagar.in/_dFRCa3T_400x400_q6rl5s.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593642/Portfolio-AmanSagar.in/Tailwind_CSS_Logo_urf3an.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593942/Portfolio-AmanSagar.in/1024px-Unofficial_JavaScript_logo_2.svg_tjajtk.jpg?_s=public-apps"], // Example icon list
+  },
+  {
+    id: 4,
+    title: "IPhoneX",
+    description: "Apple iPhone Clone: A dynamic replica of Apple's iPhone 15 Pro, showcasing various models in different colors and shapes.",
+    img: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736592925/Portfolio-AmanSagar.in/IphoneX.jpg?_s=public-apps",
+    liveLink: "https://live-link.com",
+    sourceCodeLink: "https://github.com/yourusername/iphonex",
+    // tools: ["ThreeJS", "ReactJS", "GSAP", "ViteJS", "HTML", "CSS", "NodeJS", "TailwindCSS", "JavaScript", "Git", "GitHub"],
+    iconLists: ["https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593533/Portfolio-AmanSagar.in/e4f86d2200d2d35c30f7b1494e96b9595ebc2751_lph7re.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736594440/Portfolio-AmanSagar.in/h28jJ1qM_400x400_rwzhkw.jpg?_s=public-apps", "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736593563/Portfolio-AmanSagar.in/React.js_logo-512_k7bq3r.jpg?_s=public-apps"], // Example icon list
+  },
+  // {
+  //   id: 5,
+  //   title: "Web Portfolio",
+  //   description: "Personal Web Portfolio: A dynamic portfolio featuring about, skills, projects, experiences, and contact details with animated elements.",
+  //   img: "/path/to/webportfolio_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/webportfolio",
+  //   tools: ["ReactJS", "NodeJS", "ThreeJS", "Figma", "TailwindCSS", "JavaScript", "HTML", "CSS", "Git", "GitHub", "ViteJS"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 6,
+  //   title: "Musicify",
+  //   description: "Interactive Music Player: A dynamic music player featuring user-friendly interface with API integration, enhancing track features.",
+  //   img: "/path/to/musicify_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/musicify",
+  //   tools: ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Figma"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 7,
+  //   title: "Loruki Cloud Hosting",
+  //   description: "Loruki: Your top choice for advanced cloud hosting, delivering seamless and reliable services with a user-friendly interface.",
+  //   img: "/path/to/loruki_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/loruki",
+  //   tools: ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Adobe XD"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 8,
+  //   title: "Aryan Photography",
+  //   description: "A Photography Platform: Showcasing portfolios with stunning photo shoots and captivating wild images.",
+  //   img: "/path/to/aryanphotography_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/aryanphotography",
+  //   tools: ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Adobe XD"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 9,
+  //   title: "TRIPME",
+  //   description: "TripMe: Elevate your travel experience with effortless booking and discover top-notch hotels, all in one streamlined platform.",
+  //   img: "/path/to/tripme_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/tripme",
+  //   tools: ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Figma"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 10,
+  //   title: "Js Form Validation",
+  //   description: "Efficiently validate registration form inputs to ensure accurate user information in a concise and user-friendly manner.",
+  //   img: "/path/to/jsformvalidation_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/jsformvalidation",
+  //   tools: ["HTML", "CSS", "JavaScript", "Git", "GitHub", "Adobe XD"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+  // {
+  //   id: 11,
+  //   title: "Gamify",
+  //   description: "Created a user-friendly gaming website that enables seamless browsing and discovery of various gaming products.",
+  //   img: "/path/to/gamify_image.png",
+  //   liveLink: "https://live-link.com",
+  //   sourceCodeLink: "https://github.com/yourusername/gamify",
+  //   tools: ["HTML", "CSS", "Bootstrap", "JavaScript", "Adobe XD", "Git", "GitHub"],
+  //   iconLists: ["/path/to/icon1.png", "/path/to/icon2.png"], // Example icon list
+  // },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+// data.js or a similar file
+export const workExperience = [
+  {
+    id: 1,
+    title: "Java Developer Intern",
+    company: "Kodnest",
+    duration: "June 2024 - Present",
+    thumbnail: "/exp1.svg",
+    // desc: [
+    //   "Developing full-stack applications using Java and SQL, enhancing both frontend and backend functionalities.",
+    //   "Implemented modern technologies and frameworks to optimize system design and improve performance.",
+    //   "Built scalable and optimized solutions to ensure seamless user experiences.",
+    // ],
+  },
+  {
+    id: 2,
+    title: "Web Developer Intern",
+    company: "Codsoft",
+    duration: "Dec 2023 - Jan 2024",
+    thumbnail: "/exp2.svg",
+    // desc: [
+    //   "Developed a responsive and interactive web portfolio, utilizing HTML, CSS, JavaScript, ReactJS, Node.js, and Tailwind CSS.",
+    //   "Gained hands-on expertise in React DOM and Routing strategies, contributing to the project's dynamic functionality.",
+    //   "Achieved a 20% improvement in user experience and cross-browser compatibility by 10% through cross-platform optimization.",
+    // ],
+  },
+  // {
+  //   id: 3,
+  //   title: "Web Developer",
+  //   company: "Asper",
+  //   duration: "Apr 2021 - Aug 2022",
+  //   thumbnail: "/exp3.svg",
+  //   // desc: [
+  //   //   "Actively participated in user-centric approach creation, significantly enhancing proficiency in web development methodologies.",
+  //   //   "Utilized HTML, CSS, JavaScript for streamlined code collaboration and version control, ensuring smooth project management.",
+  //   //   "Demonstrated adaptability by actively embracing emerging tools and technologies, driving innovation in development.",
+  //   // ],
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   company: "Asper",
+  //   duration: "Apr 2021 - Aug 2022",
+  //   thumbnail: "/exp4.svg",
+  //   // desc: [
+  //   //   "Developed and maintained user-facing features using modern frontend technologies.",
+  //   // ],
+  // },
+];
+
+export const education = [
+  {
+    id: 1,
+    title: "Bachelor of Technology",
+    institution: "University Institute of Technology RGPV, Bhopal",
+    duration: "2020 - 2024",
+    thumbnail: "https://iconape.com/wp-content/png_logo_vector/rajiv-gandhi-proudyogiki-vishwavidyalaya-logo.png",
+    desc: "Information Technology, CGPA: 7.15",
+  },
+  {
+    id: 2,
+    title: "Higher Secondary (XII)",
+    institution: "Scholars Public Higher Secondary School, Niwari",
+    duration: "May 2019 - April 2020",
+    thumbnail: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736595482/Portfolio-AmanSagar.in/school_qqr2z9.jpg?_s=public-apps",
+    desc: "Science, Percentage: 80.00%",
+  },
+  {
+    id: 3,
+    title: "High School (X)",
+    institution: "Rani Laxmi Bai Public Academy High School, Niwari",
+    duration: "April 2017 - April 2018",
+    thumbnail: "https://res.cloudinary.com/dl7zgwx4o/image/upload/fl_preserve_transparency/v1736595351/Portfolio-AmanSagar.in/rlpa_logo_ixnhew.jpg?_s=public-apps",
+    desc: "Science, Percentage: 91.4%",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
